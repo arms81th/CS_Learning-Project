d17e86e594e25d2654982b22dd1e0787a579c015
arms81th <takdanai8143@gmail.com>
2026-05-25 11:48:35 +0700

    Create Program.cs

---
+++ CS_learning/quiz2/quiz2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n=== Addition, subtraction, multiplication, division ===");
+            Console.Write("Enter The first set of numbers: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter The second set of numbers: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("answer (+) = {0}", num1 + num2);
+            Console.WriteLine("answer (-) = {0}", num1 - num2);
+            Console.WriteLine("answer (*) = {0}", num1 * num2);
+            Console.WriteLine("answer (/) = {0}", num1 / num2);
+            Console.WriteLine("answer (%) = {0}", num1 % num2);
+            Console.ReadLine();
+        }
+    }
+}
