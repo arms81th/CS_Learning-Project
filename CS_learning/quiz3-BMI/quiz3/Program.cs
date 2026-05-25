56b5fb0f09ba30a98e90fe96a4266ac061ebcb57
arms81th <takdanai8143@gmail.com>
2026-05-25 12:17:54 +0700

    Create Program.cs

---
+++ CS_learning/quiz3-BMI/quiz3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much does it weigh? : ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How tall is it? : ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double heightInMeters = height / 100;
+
+            double BMI = (weight /(heightInMeters * heightInMeters));
+
+            Console.WriteLine($"The BMI is :{BMI:F2}");
+            Console.ReadLine();
+
+        }
+    }
+}
